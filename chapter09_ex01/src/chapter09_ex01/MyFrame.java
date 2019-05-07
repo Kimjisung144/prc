4c3a35bf535d281af20202c6a2c6fe70ce23df81
HP Note <HP Note@192.168.25.41>
2019-05-07 10:43:49 +0900

    ㅇㄹㄴㅇㄹㄴㅇ

---
+++ chapter09_ex01/src/chapter09_ex01/MyFrame.java
@@ -0,0 +1,18 @@
+package chapter09_ex01;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		setTitle("ÀÎ»ý");
+		setSize(300, 300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFrame frame = new MyFrame();
+
+	}
+
+}
